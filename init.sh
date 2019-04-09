7552fdd622d84bff684c2f658de8ecd7fc68bec9
lisaac <lisaac.cn@gmail.com>
2019-04-09 21:50:04 +0800

    加入关闭ipv6，可添加不走代理的内网ip

--- init.sh
+++ init.sh
@@ -1,60 +1,95 @@
 #!/bin/bash
 
-if [ ! -f /version ]; then
-  /update.sh
-fi
+CONFIG_PATH="/etc/ss-tproxy"
+function check-env {
+  if [ ! -f /usr/local/bin/ss-tproxy -o ! -f /v2ray/v2ray -o ! -f /koolproxy/koolproxy  -o ! -f /sample_config/ss-tproxy.conf -o ! -f /sample_config/v2ray.conf -o ! -f /sample_config/gfwlist.ext ]; then
+    /update.sh || echo "[ERR] Can't update, please check networking or update the container. "
+  fi
+  return 0
+}
 
-CONFIG_PATH='/etc/ss-tproxy'
-NEED_EXIT=0
-# 若没有配置文件，拷贝配置文件模版
-[ ! -f "$CONFIG_PATH"/ss-tproxy.conf ] && { cp /sample_config/ss-tproxy.conf "$CONFIG_PATH"; echo "[ERR] No ss-tproxy.conf, sample file copied, please configure it."  1>&2; NEED_EXIT=1; }
-[ ! -f "$CONFIG_PATH"/v2ray.conf ] && { cp /sample_config/v2ray.conf "$CONFIG_PATH"; echo "[ERR] no v2ray.conf, sample file copied, please configure it."  1>&2; NEED_EXIT=1; }
-[ ! -f "$CONFIG_PATH"/gfwlist.ext ] && { cp /sample_config/gfwlist.ext "$CONFIG_PATH"; }
-if [ "$NEED_EXIT" = 1 ]; then
-  exit 1;
-fi
+function check-config {
+  NEED_EXIT="false"
+  # 若没有配置文件，拷贝配置文件模版
+  if [ ! -f "$CONFIG_PATH"/ss-tproxy.conf ]; then
+    cp /sample_config/ss-tproxy.conf "$CONFIG_PATH"
+    echo "[ERR] No ss-tproxy.conf, sample file copied, please configure it."
+    NEED_EXIT="true"
+  fi
+  if [ ! -f "$CONFIG_PATH"/v2ray.conf ]; then
+    cp /sample_config/v2ray.conf "$CONFIG_PATH"
+    echo "[ERR] No v2ray.conf, sample file copied, please configure it."
+    NEED_EXIT="true"
+  fi
+  if [ ! -f "$CONFIG_PATH"/gfwlist.ext ]; then
+    cp /sample_config/gfwlist.ext "$CONFIG_PATH"
+  fi
+  if [ "$NEED_EXIT" = "true" ]; then
+    exit 1;
+  fi
+  # touch 空配置文件
+  source "$CONFIG_PATH"/ss-tproxy.conf
+  [ ! -f "$file_gfwlist_txt" ] && touch $file_gfwlist_txt
+  [ ! -f "$file_chnroute_txt" ] && touch $file_chnroute_txt
+  [ ! -f "$file_chnroute_set" ] && touch $file_chnroute_set
+  [ ! -f "$dnsmasq_addn_hosts" ] && touch $dnsmasq_addn_hosts
+  return 0
+}
 
-# touch 空配置文件
-source "$CONFIG_PATH"/ss-tproxy.conf
-[ ! -f "$file_gfwlist_txt" ] && touch $file_gfwlist_txt
-[ ! -f "$file_chnroute_txt" ] && touch $file_chnroute_txt
-[ ! -f "$file_chnroute_set" ] && touch $file_chnroute_set
-[ ! -f "$dnsmasq_addn_hosts" ] && touch $dnsmasq_addn_hosts
+function stop-ss-tproxy {
+  # 更新之前，停止 ss-tproxy
+  echo "`date +%Y-%m-%d\ %T` stopping tproxy-gateway.."
+  /usr/local/bin/ss-tproxy stop && return 0
+}
 
-# 更新之前，停止 ss-tproxy
-echo "`date +%Y-%m-%d\ %T` stopping tproxy-gateway.."
-/usr/local/bin/ss-tproxy stop
-# 更新 ss-tproxy 配置
-if [ "$mode" = chnroute ]; then
-  echo "`date +%Y-%m-%d\ %T` updating chnroute.."
-  /usr/local/bin/ss-tproxy update-chnroute
-fi
-if [ "$mode" = gfwlist ]; then
-  echo "`date +%Y-%m-%d\ %T` updating gfwlist.."
-  /usr/local/bin/ss-tproxy update-gfwlist
-fi
-if [ "$mode" = chnonly ]; then
-  echo "`date +%Y-%m-%d\ %T` updating chnonly.."
-  /usr/local/bin/ss-tproxy update-chnonly
-fi
-# 清除 iptables
-echo "`date +%Y-%m-%d\ %T` flushing iptables.."
-/usr/local/bin/ss-tproxy flush-iptables
-# 清除 gfwlist
-echo "`date +%Y-%m-%d\ %T` flushing gfwlist.."
-/usr/local/bin/ss-tproxy flush-gfwlist
-# 清除 dns cache
-echo "`date +%Y-%m-%d\ %T` flushing dnscache.."
-/usr/local/bin/ss-tproxy flush-dnscache
-# 停止 crond
-kill -9 $(pidof crond) & > /dev/null
-# 若 /etc/crontabs/root 存在 '/init.sh' 则启动 crond
-grep -n '^[^#]*/init.sh' /etc/crontabs/root && crond
-# 启动 ss-tproxy
-echo "`date +%Y-%m-%d\ %T` staring tproxy-gateway.."
-/usr/local/bin/ss-tproxy start && \
+function update-ss-config {
+  # 更新 ss-tproxy 配置
+  if [ "$mode" = chnroute ]; then
+    echo "`date +%Y-%m-%d\ %T` updating chnroute.."
+    /usr/local/bin/ss-tproxy update-chnroute
+  elif [ "$mode" = gfwlist ]; then
+    echo "`date +%Y-%m-%d\ %T` updating gfwlist.."
+    /usr/local/bin/ss-tproxy update-gfwlist
+  elif [ "$mode" = chnonly ]; then
+    echo "`date +%Y-%m-%d\ %T` updating chnonly.."
+    /usr/local/bin/ss-tproxy update-chnonly
+  fi && \
+  return 0
+}
+
+function flush-ss-tproxy {
+  # 清除 iptables
+  echo "`date +%Y-%m-%d\ %T` flushing iptables.."
+  /usr/local/bin/ss-tproxy flush-iptables
+  # 清除 gfwlist
+  echo "`date +%Y-%m-%d\ %T` flushing gfwlist.."
+  /usr/local/bin/ss-tproxy flush-gfwlist
+  # 清除 dns cache
+  echo "`date +%Y-%m-%d\ %T` flushing dnscache.."
+  /usr/local/bin/ss-tproxy flush-dnscache
+  return 0
+}
+
+function set-cron {
+  # 停止 crond
+  cron_pid="`pidof crond`"
+  if [ -n "$cron_pid" ]; then
+    kill -9 "$cron_pid" &> /dev/null
+  fi
+  # 若 /etc/crontabs/root 存在 '/init.sh' 则启动 crond
+  grep -n '^[^#]*/init.sh' /etc/crontabs/root && crond
+  return 0
+}
+
+function start-ss-tproxy {
+  # 启动 ss-tproxy
+  echo "`date +%Y-%m-%d\ %T` staring tproxy-gateway.."
+  /usr/local/bin/ss-tproxy start && return 0
+}
+
+check-env && check-config && stop-ss-tproxy && update-ss-config && flush-ss-tproxy && set-cron && start-ss-tproxy && \
 echo -e "IPv4 gateway & dns server: \n`ip addr show eth0 |grep 'inet ' | awk '{print $2}' |sed 's/\/.*//g'`" && \
-echo -e "IPv6 dns server: \n`ip addr show eth0 |grep 'inet6 ' | awk '{print $2}' |sed 's/\/.*//g'`"
+echo -e "IPv6 dns server: \n`ip addr show eth0 |grep 'inet6 ' | awk '{print $2}' |sed 's/\/.*//g'`" || echo "[ERR] Start tproxy-gateway failed."
 if [ "$1" = daemon ]; then
   tail -f /dev/null
 fi
--- update.sh
+++ update.sh
@@ -1,78 +1,100 @@
 #!/bin/bash
 
-arch=`uname -m`
-v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
-if [ "$arch" = "x86_64" ]; then
-  kp_url="https://koolproxy.com/downloads/x86_64"
-  v2ray_url="https://raw.githubusercontent.com/v2ray/dist/master/v2ray-linux-64.zip"
-  #v2ray_url="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-64.zip"
-fi
-if [ "$arch" = "aarch64" ]; then
-  kp_url="https://koolproxy.com/downloads/arm"
-  v2ray_url="https://raw.githubusercontent.com/v2ray/dist/master/v2ray-linux-arm64.zip"
-  #v2ray_url="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-arm64.zip"
-fi
+function get-link {
+  echo "`date +%Y-%m-%d\ %T` Getting latest version."
+  arch=`uname -m`
+  v2ray_latest_ver="$(curl -H 'Cache-Control: no-cache' -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
+  if [ "$arch" = "x86_64" ]; then
+    kp_url="https://koolproxy.com/downloads/x86_64"
+    v2ray_url="https://raw.githubusercontent.com/v2ray/dist/master/v2ray-linux-64.zip"
+    #v2ray_url="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-64.zip"
+  elif [ "$arch" = "aarch64" ]; then
+    kp_url="https://koolproxy.com/downloads/arm"
+    v2ray_url="https://raw.githubusercontent.com/v2ray/dist/master/v2ray-linux-arm64.zip"
+    #v2ray_url="https://github.com/v2ray/v2ray-core/releases/download/$v2ray_latest_ver/v2ray-linux-arm64.zip"
+  fi
+  ss_url="https://raw.githubusercontent.com/zfl9/ss-tproxy/v3-master/ss-tproxy"
+  chinadns_url="https://raw.githubusercontent.com/lisaac/tproxy-gateway/master/chinadns.`uname -m`"
+  echo "`date +%Y-%m-%d\ %T` V2ray latest version is $v2ray_latest_ver."
+}
 
 # 更新之前停止 ss-tproxy
-if [ -f /usr/local/bin/ss-tproxy ]; then
-  /usr/local/bin/ss-tproxy stop > /dev/null
-fi
+function stop-sstorpxy {
+  if [ -f /usr/local/bin/ss-tproxy ]; then
+    /usr/local/bin/ss-tproxy stop > /dev/null
+  fi
+}
 
 # 更新系统
-echo "`date +%Y-%m-%d\ %T` Upgrading system.."
-sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories && \
-apk --no-cache --no-progress upgrade && \
-apk --no-cache --no-progress add perl curl bash iptables pcre openssl dnsmasq ipset iproute2 tzdata && \
-sed -i 's/mirrors.aliyun.com/dl-cdn.alpinelinux.org/g' /etc/apk/repositories
+function update-system {
+  echo "`date +%Y-%m-%d\ %T` Upgrading system.."
+  sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories && \
+  apk --no-cache --no-progress upgrade && \
+  apk --no-cache --no-progress add perl curl bash iptables pcre openssl dnsmasq ipset iproute2 tzdata && \
+  sed -i 's/mirrors.aliyun.com/dl-cdn.alpinelinux.org/g' /etc/apk/repositories
+}
 
-# 更新 V2RAY 版本
-echo "`date +%Y-%m-%d\ %T` Updating v2ray.."
-if [ -f /v2ray/v2ray ]; then
-  v2ray_latest_v="$(echo $v2ray_latest_ver | cut -dv -f2)"
-  v2ray_current_v="$(/v2ray/v2ray -version | grep V2Ray | cut -d' ' -f2)"
-fi
-if [ "$v2ray_latest_v" != "$v2ray_current_v" -o ! -f /v2ray/v2ray ]; then
-  rm -fr /v2ray && mkdir -p /v2ray && cd /v2ray && \
-  wget "$v2ray_url" -O v2ray-linux.zip && \
-  unzip v2ray-linux.zip && \
-  rm -fr doc systemv systemd config.json v2ray-linux.zip && \
-  chmod +x v2ray v2ctl
-fi
+# 更新 V2RAY
+function update-v2ray {
+  echo "`date +%Y-%m-%d\ %T` Updating v2ray.."
+  if [ -f /v2ray/v2ray ]; then
+    v2ray_latest_v="$(echo $v2ray_latest_ver | cut -dv -f2)"
+    v2ray_current_v="$(/v2ray/v2ray -version | grep V2Ray | cut -d' ' -f2)"
+  fi
+  if [ "$v2ray_latest_v" != "$v2ray_current_v" -o ! -f /v2ray/v2ray ]; then
+    rm -fr /v2ray && mkdir -p /v2ray && cd /v2ray && \
+    wget "$v2ray_url" -O v2ray-linux.zip && \
+    unzip v2ray-linux.zip && \
+    rm -fr doc systemv systemd config.json v2ray-linux.zip && \
+    chmod +x v2ray v2ctl
+  fi
+}
+
 
 # 更新 ss-tproxy 并 patch
-echo "`date +%Y-%m-%d\ %T` Updating ss-tproxy.."
-cd / && mkdir -p /ss-tproxy &&\
-wget https://raw.githubusercontent.com/zfl9/ss-tproxy/v3-master/ss-tproxy -O /ss-tproxy/ss-tproxy && \
-sed -i 's/while umount \/etc\/resolv.conf; do :; done/while mount|grep overlay|grep \/etc\/resolv.conf; do umount \/etc\/resolv.conf; done/g' /ss-tproxy/ss-tproxy && \
-sed -i 's/60053/53/g' /ss-tproxy/ss-tproxy && \
-sed -i '/no-resolv/i\addn-hosts=$dnsmasq_addn_hosts' /ss-tproxy/ss-tproxy && \
-install -c /ss-tproxy/ss-tproxy /usr/local/bin && \
-mkdir -m 0755 -p /etc/ss-tproxy && chown -R root:root /etc/ss-tproxy && \
-rm -rf /ss-tproxy
+function update-ss-tproxy {
+  echo "`date +%Y-%m-%d\ %T` Updating ss-tproxy.."
+  cd / && mkdir -p /ss-tproxy &&\
+  wget "$ss_url" -O /ss-tproxy/ss-tproxy && \
+  sed -i 's/while umount \/etc\/resolv.conf; do :; done/while mount|grep overlay|grep \/etc\/resolv.conf; do umount \/etc\/resolv.conf; done/g' /ss-tproxy/ss-tproxy && \
+  sed -i 's/60053/53/g' /ss-tproxy/ss-tproxy && \
+  sed -i '/no-resolv/i\addn-hosts=$dnsmasq_addn_hosts' /ss-tproxy/ss-tproxy && \
+  install -c /ss-tproxy/ss-tproxy /usr/local/bin && \
+  mkdir -m 0755 -p /etc/ss-tproxy && chown -R root:root /etc/ss-tproxy && \
+  rm -rf /ss-tproxy
+}
 
 # 更新 koolproxy
-echo "`date +%Y-%m-%d\ %T` Updating koolproxy.."
-rm -fr /koolproxy && mkdir -p /koolproxy && cd /koolproxy && \
-wget "$kp_url" -O /koolproxy/koolproxy && \
-chmod +x /koolproxy/koolproxy && \
-chown -R daemon:daemon /koolproxy
+function update-koolproxy {
+  echo "`date +%Y-%m-%d\ %T` Updating koolproxy.."
+  rm -fr /koolproxy && mkdir -p /koolproxy && cd /koolproxy && \
+  wget "$kp_url" -O koolproxy && chmod +x /koolproxy/koolproxy && chown -R daemon:daemon /koolproxy
+}
 
 # 更新 chinadns
-echo "`date +%Y-%m-%d\ %T` Updating chinadns.."
-if [ ! -f /usr/local/bin/chinadns ]; then
-  wget https://raw.githubusercontent.com/lisaac/tproxy-gateway/master/chinadns.`uname -m` -O /tmp/chinadns && install -c /tmp/chinadns /usr/local/bin
-fi
+function update-chinadns {
+  echo "`date +%Y-%m-%d\ %T` Updating chinadns.."
+  if [ ! -f /usr/local/bin/chinadns ]; then
+    wget "$chinadns_url" -O /tmp/chinadns && install -c /tmp/chinadns /usr/local/bin
+  fi
+}
 
-echo "`date +%Y-%m-%d\ %T` Updating sample files.."
-mkdir -p /sample_config
-wget https://raw.githubusercontent.com/lisaac/tproxy-gateway/master/ss-tproxy.conf -O /sample_config/ss-tproxy.conf
-wget https://raw.githubusercontent.com/lisaac/tproxy-gateway/master/v2ray.conf -O /sample_config/v2ray.conf
-wget https://raw.githubusercontent.com/lisaac/tproxy-gateway/master/gfwlist.ext -O /sample_config/gfwlist.ext
+function update-sample-files {
+  echo "`date +%Y-%m-%d\ %T` Updating sample files.."
+  mkdir -p /sample_config
+  wget https://raw.githubusercontent.com/lisaac/tproxy-gateway/master/ss-tproxy.conf -O /sample_config/ss-tproxy.conf && \
+  wget https://raw.githubusercontent.com/lisaac/tproxy-gateway/master/v2ray.conf -O /sample_config/v2ray.conf && \
+  wget https://raw.githubusercontent.com/lisaac/tproxy-gateway/master/gfwlist.ext -O /sample_config/gfwlist.ext
+}
 
 # 写入更新日期及版本
-rm -rf /tmp/*
-echo "Update completed !!"
-echo "Update time: `date +%Y-%m-%d\ %T`" > /version
-echo "V2Ray version: `/v2ray/v2ray -version | grep V2Ray | cut -d' ' -f2`" | tee -a /version
-echo "Koolproxy version: `/koolproxy/koolproxy -v`" | tee -a /version
-echo "Chinadns version: `/usr/local/bin/chinadns -V | cut -d' ' -f2`" | tee -a /version
+function check-version {
+  rm -rf /tmp/*
+  echo "Update time: `date +%Y-%m-%d\ %T`" > /versions
+  echo "V2Ray version: `/v2ray/v2ray -version | grep V2Ray | cut -d' ' -f2`" | tee -a /version
+  echo "Koolproxy version: `/koolproxy/koolproxy -v`" | tee -a /version
+  echo "Chinadns version: `/usr/local/bin/chinadns -V | cut -d' ' -f2`" | tee -a /version
+  echo "Update completed !!"
+}
+
+get-link && stop-sstorpxy && update-v2ray &&  update-ss-tproxy && update-chinadns && update-sample-files && check-version || echo "Update failed." 
